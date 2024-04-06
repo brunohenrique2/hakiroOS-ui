72f960ae0c26c3e440c9c3789dd263e31569c7cb
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-04-06 11:12:00 -0300

    adicionado as opções de energia

--- js/win.js
+++ js/win.js
@@ -1,7 +1,14 @@
 let winStart = document.querySelector("#winStart")
 let btnStart = document.querySelector("#start")
+let powerSettingsBox = document.querySelector("#powerSettingsBox")
+let btnPower = document.querySelector("#powerSettings")
 
 btnStart.addEventListener("click", () => {
     winStart.classList.toggle("active")
+    powerSettingsBox.classList.remove("active")
     console.log("click")
+})
+
+btnPower.addEventListener("click", () => {
+    powerSettingsBox.classList.toggle("active")
 })
