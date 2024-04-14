3e28a9781a60e0563e0f27ccdb22206f78c1f81e
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-04-13 22:59:05 -0300

    janelas dinamicas!

---
+++ apps/settings/settings.js
@@ -0,0 +1,109 @@
+let appSettings = document.querySelector("#settings")
+
+const winSettings = new Windows(appSettings.id)
+
+appSettings.addEventListener("click", () => { 
+    winSettings.openWindow(appSettings.id)
+    winStart.classList.remove("active")
+})
+
+let winClose = document.querySelectorAll(".winClose")
+let windows = document.querySelectorAll(".window")
+
+for(let i = 0; i < winClose.length; i++){
+    winClose[i].addEventListener("click", () => {
+        for(let i = 0; i < windows.length; i++) {
+            if(windows[0].id == appSettings.id) {
+                windows[0].classList.remove("active")
+            }
+        }
+    })
+}
+
+for(let i = 0; i < windows.length; i++) {
+    if(windows[0].id == appSettings.id) {
+        const winSettingsBar = document.createElement("div")
+        const themes = document.createElement("div")
+        const optionThemesIcon = document.createElement("span")
+        const optionThemesName = document.querySelector("p")
+        const winSettingsTheme = document.createElement("div")
+        const settingsThemeTittle = document.createElement("h1")
+        const selectTheme = document.createElement("div")
+        const lightMode = document.createElement("div")
+        const lightModeTittle = document.createElement("p")
+        const darkModeTittle = document.createElement("p")
+        const darkMode = document.createElement("div")
+
+
+
+        winSettingsBar.setAttribute("id", "winSettingsBar")
+        themes.setAttribute("id", "themes")
+        themes.classList.add("winSettingsOptions")
+        optionThemesIcon.classList.add("material-symbols-rounded")
+        optionThemesIcon.innerHTML = "draw"
+        optionThemesName.innerHTML = "Costumer"
+        winSettingsTheme.setAttribute("id", "winSettingsTheme")
+        settingsThemeTittle.setAttribute("id", "settingsThemeTittle")
+        settingsThemeTittle.innerHTML = "Themes"
+        selectTheme.setAttribute("id", "selectTheme")
+        lightMode.setAttribute("id", "lightMode")
+        lightMode.classList.add("themeOptions")
+        lightModeTittle.classList.add("nameTheme")
+        lightModeTittle.innerHTML = "Light"
+        darkMode.setAttribute("id", "darkMode")
+        darkMode.classList.add("themeOptions")
+        darkModeTittle.classList.add("nameTheme")
+        darkModeTittle.innerHTML = "Dark"
+
+        
+        windows[0].appendChild(winSettingsBar)
+        winSettingsBar.appendChild(themes)
+        themes.appendChild(optionThemesIcon)
+        themes.appendChild(optionThemesName)
+        winSettingsBar.appendChild(winSettingsTheme)
+        winSettingsTheme.appendChild(settingsThemeTittle)
+        winSettingsTheme.appendChild(selectTheme)
+        selectTheme.appendChild(lightMode)
+        lightMode.appendChild(lightModeTittle)
+        selectTheme.appendChild(darkMode)
+        darkMode.appendChild(darkModeTittle)
+    }
+}
+
+let btnTheme = document.querySelector("#themes")
+let winSettingsTheme = document.querySelector("#winSettingsTheme")
+
+btnTheme.addEventListener("click", () => {
+    winSettingsTheme.classList.add("active")
+})
+
+/*
+`
+<div id="winSettingsBar">
+    <div id="themes" class="winSettingsOptions">
+        <span class="material-symbols-rounded">draw</span>
+        <p>Theme</p>
+    </div>
+</div>
+<div id="winSettingsTheme">
+    <h2 id="settingsThemeTittle">Theme</h2>
+    <div id="selectTheme">
+        <div id="lightMode" class="themeOptions">
+            <p class="nameTheme">Light</p>
+        </div>
+        <div id="darkMode" class="themeOptions">
+            <p class="nameTheme">Dark</p>
+        </div>
+    </div>
+    <!--
+        <div id="selectWallpaper">
+            <h2 id="settingsThemeTittle">Wallpaper</h2>
+            <label id="btnAddWallpaper" for="addWallpaper">
+                <span class="material-symbols-rounded">add</span>
+                <input type="file" name="addWallpaper" id="addWallpaper">
+            </label>
+        </div>
+    -->
+</div>
+`
+*/
--- js/window.js
+++ js/window.js
@@ -14,24 +14,112 @@ btnPower.addEventListener("click", () => {
     powerSettingsBox.classList.toggle("active")
 })
 
-/*====================JANELA DE CONFIGURAÇÕES====================*/
+/*====================JANELAS====================*/
 
-let appSettings = document.querySelector("#settings")
-let winSettings = document.querySelector("#winSettings")
-let winClose = document.querySelector("#winClose")
-let btnTheme = document.querySelector("#themes")
-let winSettingsTheme = document.querySelector("#winSettingsTheme")
 let addWallpaper = document.querySelector("#addWallpaper")
 
+
+class Windows {
+    constructor(id) {
+        
+        const main = document.querySelector("#main")
+        const window = document.createElement("div")
+        const winTittleBar = document.createElement("div")
+        const winTittle = document.createElement("div")
+        const winOptionsBox = document.createElement("div")
+        const btnClose = document.createElement("span")
+        const btnResize = document.createElement("span")
+        const btnMinimize = document.createElement("span")
+        const iconApp = document.createElement("span")
+
+        btnClose.innerHTML = "close"
+        btnResize.innerHTML = "fullscreen"
+        btnMinimize.innerHTML = "remove"
+        iconApp.innerHTML = "settings"
+
+        window.id = id
+
+        window.classList.add("window")
+        winTittleBar.classList.add("winTittleBar")
+        winTittle.classList.add("winTittle")
+        winOptionsBox.classList.add("winOptionsBox")
+        btnClose.classList.add("material-symbols-rounded", "winOptions", "winClose")
+        btnResize.classList.add("material-symbols-rounded", "winOptions", "winResize")
+        btnMinimize.classList.add("material-symbols-rounded", "winOptions", "winMinimize")
+        iconApp.classList.add("material-symbols-rounded")
+
+        main.appendChild(window)
+        window.appendChild(winTittleBar)
+        winTittleBar.appendChild(winTittle)
+        winTittle.appendChild(iconApp)
+        winTittleBar.appendChild(winOptionsBox)
+        winOptionsBox.appendChild(btnClose)
+        winOptionsBox.appendChild(btnResize)
+        winOptionsBox.appendChild(btnMinimize)
+    }
+
+    openWindow(id) {
+        let windows = document.querySelectorAll(".window")
+
+        for(var i = 0; i < windows.length; i++) {
+            if(windows[i].id == id) {
+                windows[i].classList.add("active")
+            }
+        }
+    }
+
+    closeWindow(id) {
+       
+    }
+
+    resizeWindow() {
+        for(i = 0; i < winResize.length; i++) {
+            winResize[i].addEventListener("click", () => {
+                winResize.innerHTML = "fullscreen_exit"
+            })
+        }
+    }
+}
+
+/*
 appSettings.addEventListener("click", () => {
-    winSettings.classList.add("active")
+    for(i = 0; i < windows.length; i++) {
+        windows[i].classList.add("active")
+        windows[i].appendChild = 
+        `
+        <div id="winSettingsBar">
+            <div id="themes" class="winSettingsOptions">
+                <span class="material-symbols-rounded">draw</span>
+                <p>Theme</p>
+            </div>
+        </div>
+        <div id="winSettingsTheme">
+            <h2 id="settingsThemeTittle">Theme</h2>
+            <div id="selectTheme">
+                <div id="lightMode" class="themeOptions">
+                    <p class="nameTheme">Light</p>
+                </div>
+                <div id="darkMode" class="themeOptions">
+                    <p class="nameTheme">Dark</p>
+                </div>
+            </div>
+            <!--
+                <div id="selectWallpaper">
+                    <h2 id="settingsThemeTittle">Wallpaper</h2>
+                    <label id="btnAddWallpaper" for="addWallpaper">
+                        <span class="material-symbols-rounded">add</span>
+                        <input type="file" name="addWallpaper" id="addWallpaper">
+                    </label>
+                </div>
+            -->
+        </div>
+        `
+
+    }
+    
     winStart.classList.remove("active") 
+
 })
 
-winClose.addEventListener("click", () => {
-    winSettings.classList.remove("active")
-})
 
-btnTheme.addEventListener("click", () => {
-    winSettingsTheme.classList.add("active")
-})
+*/
