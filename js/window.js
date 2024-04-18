ccd8db68126a301fa6839106c1b1983c6652204a
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2024-04-17 22:56:24 -0300

    versão 0.6.3b

--- js/window.js
+++ js/window.js
@@ -144,3 +144,12 @@ for(let i = 0; i < winTittleBar.length; i++){
     })
 }
 
+ var matriz = [ [1,2,3],
+                [4,5,6],
+                [7,8,9] ];
+
+    for(let lin=0 ; lin<3 ; lin++){
+        for(let col=0; col<3 ; col++){
+            console.log(matriz[lin][col]);
+        }
+    }
